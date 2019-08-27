e87757faa82415596039e4069f1b35860cb4853b
Voufack <54106683+Voufack@users.noreply.github.com>
2019-08-27 12:55:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@
 
 const express = require('express');
 const app = express();
-const port = 2500;
+const port = 3000;
 
-app.get('/',(req,res) =>{
-  res.send('hello world');
+app.get('/hello',(req,res) =>{
+  res.send('world! ');
 });
 
 app.listen(port);
